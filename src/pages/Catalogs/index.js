528f673c74a7358ed5719f9732ac6d62cd36d8a5
Ja'farxon <saidumarovjafarxon@gmail.com>
2023-10-03 16:14:12 +0500

    first commit

--- src/pages/Catalogs/index.js
+++ src/pages/Catalogs/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Cards from "../../components/sections/Cards";
-import { Catalogtitle } from "../../components/sections/Title/index";
 import style from "../../components/sections/Title/index.module.scss"
 import { ImageSliderdata } from "../../components/sections/Slider/indexData";
 import { Catalogmenyu } from "./catalogmenyu/index";
+import { Catalogtitle } from "../../components/sections/Title";
 function Catalogs() {
   return (
     <>
