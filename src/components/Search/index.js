5f5cbf66eb999ce4ae55ab5c87e66765eee2ff06
Saidumarov <saidumarovjafarxon06@gmail.com>
2023-10-29 15:50:58 +0500

    api

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -5,14 +5,14 @@ import myAxios from "../../urlAPI/index";
 import { NotFound } from "../../pages/NotFound";
 import ProductCard from "../ProductCard"; // Import the ProductCard component
 import Loader from "../sections/Loader";
-
+import styles from "./index.module.scss";
 function SearchInfo() {
   const { value } = useContext(Context);
   const { data, isLoading, isError } = useQuery(["product", value], () =>
     // myAxios.get(`/api/product?search=${value || ""}`)
     myAxios.get(`/api/product`)
   );
-
+  console.log(data?.data?.data?.products);
   if (isLoading) {
     return (
       <div>
@@ -25,22 +25,14 @@ function SearchInfo() {
     return <NotFound />;
   }
 
-  if (!data || !data.data || !data.data.products) {
-    return null; // Return null or a placeholder if the data is missing
-  }
-  console.log(value);
-
   return (
-    <div>
-      <div>
-        <h1>{value}</h1>
-      </div>
-      <div>
-        {data.products.map((item) => (
+    <>
+      <div className={styles.serch_wrapper}>
+        {data?.data?.data?.products.map((item) => (
           <ProductCard key={item._id} product={item} />
         ))}
       </div>
-    </div>
+    </>
   );
 }
 
